16af25bca577f132d05f19f79f2cb20a8c092256
Thomas Leonardo <thomas.lrpm@gmail.com>
2018-07-24 21:40:10 -0300

    Begin quiz project

---
+++ js/questions.js
@@ -0,0 +1,26 @@
+var questions = [{
+  question: "O nome Olinda foi dado a partir de uma frase dita por: ",
+  choices: ['Duarte Coelho', 'João de Barros', 'Pedro Lopes de Souza', 'Martin Afonso'],
+  correctAnswer: 0,
+  answerLocation: '"linkaqui1"'
+}, {
+  question: "Qual era o nome da aldeia localizada onde Olinda é hoje?",
+  choices: ['Guarani','Marim','Kaiowá','Wajãpi'],
+  correctAnswer: 1,
+  answerLocation: '"linkaqui2"'
+}, {
+  question: "Quantas bacias hidrográficas Olinda possui?",
+  choices: [1, 'Nenhuma', 2, 3],
+  correctAnswer: 2,
+  answerLocation: '"linkaqui3"'
+}, {
+  question: "Qual o nome do país que invadiu Olinda em 1630?",
+  choices: ['Portugal', 'Espanha', 'Alemanha', 'Holanda'],
+  correctAnswer: 3,
+  answerLocation: '"linkaqui4"'
+}, {
+  question: "Qual desses títulos Olinda não possui?",
+  choices: ['Capital Brasileira da Cultura', 'Monumento Internacional', 'Patrimônio Histórico da Humanidade', 'Cidade Ecológica'],
+  correctAnswer: 1,
+  answerLocation: '"linkaqui5"'
+}];
